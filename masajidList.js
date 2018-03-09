09ca3e4109520ceea52d1d352905ba319489d492
Faizan Rauf <faizan.rauf@deluxe.com>
2018-03-09 08:31:05 -0600

    added masjid folders

--- masajidList.js
+++ masajidList.js
@@ -1,9 +1,9 @@
 var masajidList = {
     "masjid-taqwa":{
         "dailyprayer":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=0&single=true&output=csv",
-        "jumma":"",
-        "event":"",
-        "announcement":""
-
+        "jumma":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=160505375&single=true&output=csv",
+        "event":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=1679621038&single=true&output=csv",
+        "announcement":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=697903229&single=true&output=csv",
+        "xlsx":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?output=xlsx"
     }
 }
