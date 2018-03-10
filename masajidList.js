74e906f48bec158e144795ee90e5580ffdcf8e54
akaban01 <akaban01@gmail.com>
2018-03-09 18:39:40 -0600

    Update masajidList.js

--- masajidList.js
+++ masajidList.js
@@ -1,4 +1,4 @@
-var masajidList = {
+{
     "masjid-taqwa":{
         "dailyprayer":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=0&single=true&output=csv",
         "jumma":"https://docs.google.com/spreadsheets/d/e/2PACX-1vTtTotyMaOJLmoLQCgHv1bIueVlXdUNS8iBJBXeI4zLWe8Txw5egQTQeYSYiwiB5fRJ2l6niJphlqrz/pub?gid=160505375&single=true&output=csv",
