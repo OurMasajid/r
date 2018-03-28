a084611595bf0e7702c727ed742554705090d5b2
Faizan Rauf <faizan.rauf@deluxe.com>
2018-03-28 12:52:03 -0500

    bug fixes

--- monthly.js
+++ monthly.js
@@ -2,7 +2,7 @@ function loader(vartosaveto, url, callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        eval("vartosaveto = this.responseText");
+        eval(vartosaveto +"="+ this.responseText);
         eval(callback);
       }
     };
