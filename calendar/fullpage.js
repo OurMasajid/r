aa4a1147d36d8ae08e100fb4a37b834162158a44
Faizan Rauf <faizan.rauf@deluxe.com>
2018-04-15 00:11:23 -0500

    just duration left

--- calendar/fullpage.js
+++ calendar/fullpage.js
@@ -16,18 +16,69 @@ var tobedeleted = {
 tobedeleted["dataurl"] = "https://ourmasajid.github.io/d/icrr-cal.json";
 tobedeleted["data"]= "";
 tobedeleted["dayHTML"]= "<div class='py-3'><div class='container'><div class='card'><div class='card-header lead'> {date}</div><div class='card-body'> {events}</div></div></div></div>";
-tobedeleted["eventHTML"]= "<div class='row'><div class='col-3' style='padding-right:0'><div class=''><div class='eventtime' style='font-size: 26px;'>{time}</div><div style='margin-top: -10px'>{ampm}</div> <span class='eventtime' style='font-size: 26px; color: gray;'>{duration}</span></div></div><div class='col-9' style='padding-left:5px'><h5 class='card-title mb-0'>{title}</h5><p class='mt-0' style='font-size: 14px; color:gray;'>{location}</p><p class='card-text'> {description}</p></div></div><hr>";
+tobedeleted["eventHTML"]= "<div class='row'><div class='col-3' style='padding-right:0'><div class=''><div class='eventtime' style='font-size: 26px;'>{time}</div><div style='margin-top: -10px'>{ampm}</div> <span class='eventtime' style='font-size: 26px; color: gray;'>{duration}</span></div></div><div class='col-9' style='padding-left:5px'><h5 class='card-title mb-0'>{title}</h5><p class='mt-0' style='font-size: 14px; color:gray;'>{location}</p><p class='card-text'> {description}</p></div></div>";
 
 /*program start here*/
 showLoading();
 loader("tobedeleted.data", tobedeleted.dataurl, "createHTML()");
 
 function createHTML(){
+ let resultHTML = "";
  const dayHTML = tobedeleted["dayHTML"];
  const eventHTML = tobedeleted["eventHTML"];
  let data = tobedeleted["data"];
+ let lastDate = "";
  for (let i = 0; i < data.length; i++) {
   const obj = data[i];
-    
+  if (lastDate != obj["starttime"].substring(0,10)) {
+   resultHTML=resultHTML.replace("<hr>{events}","");
+   resultHTML+=dayHTML;
+   let niceDate = getNiceDate(obj["starttime"]);
+   resultHTML=resultHTML.replace("{date}",niceDate);
+   resultHTML=resultHTML.replace("{events}",eventHTML+"<hr>{events}");
+  }
+  else{
+   resultHTML=resultHTML.replace("{events}",eventHTML+"<hr>{events}");
+  }
+
+  resultHTML=resultHTML.replace("{time}",getStarttime(obj["starttime"]));
+  resultHTML=resultHTML.replace("{ampm}",getAmpm(obj["starttime"]));
+  resultHTML=resultHTML.replace("{duration}",getDuration(obj["starttime"], obj["endtime"]));
+  resultHTML=resultHTML.replace("{title}",obj["title"]);
+  resultHTML=resultHTML.replace("{location}",obj["location"]);
+  resultHTML=resultHTML.replace("{description}",obj["description"]);
+
+  lastDate = obj["starttime"].substring(0,10);
  }
+ resultHTML=resultHTML.replace("<hr>{events}","");
+ $("body").append(resultHTML);
+ hideLoading();
+}
+function getNiceDate(dateString){
+ return new Date(dateString).toString().substring(0,15);
+}
+function getStarttime(dateString){
+ let min = new Date(dateString).toString().substring(18,21);
+ let result = new Date(dateString).toString().substring(16,18);
+ if (result>12) {
+  result = result - 12;
+ }
+ return result+min;
+}
+function getAmpm(dateString){
+ let result = new Date(dateString).toString().substring(16,18);
+ if (result>12) {
+  return "p.m.";
+ }
+ return "a.m.";
+}
+function getDuration(startString, endString){
+ let dates = new Date(startString);
+ let datee = new Date(endString);
+ let diffmili = datee-dates;
+ let minutesT = diffmili/60000;
+ if (minutesT<60) {
+  return minutesT+"m"
+ }
+ return "todo";
 }
