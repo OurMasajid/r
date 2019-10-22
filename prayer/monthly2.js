568f81d33b21df03ff5336746be976ed08574740
akaban01 <akaban01@gmail.com>
2019-10-21 20:49:52 -0500

    updated

--- prayer/monthly2.js
+++ prayer/monthly2.js
@@ -1,4 +1,3 @@
-/* TODO when months dont have 31 days hide extra rows */
 function loader(vartosaveto, url, callback) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -26,7 +25,7 @@ loader("tobedeleted.data", tobedeleted.dataurl, "createHTML()");
 selectedDate = new Date();
 
 if (params.has('month') === true) {
-    selectedDate.setMonth(parseInt(params.get("month")-1));
+    selectedDate.setMonth(parseInt(params.get("month") - 1));
 }
 else {
     selectedDate.setDate(selectedDate.getDate() + 2);
@@ -44,59 +43,74 @@ function createHTML() {
     let data = tobedeleted["data"]["Daily Prayer"]["data"];
     let month = selectedDate.getMonth() + 1;
     let dayNumber = 0;
-    // $(".Date")[1].html("wow");
     for (let i = 0; i < data.length; i++) {
         if (month == data[i]["Month"]) {
             dayNumber++;
-            $($(".Date").get(dayNumber)).text(getDateToShow(data[i]["Month"],data[i]["Date"]));
-            $($(".Day").get(dayNumber)).text(getDayToShow(data[i]["Month"],data[i]["Date"]));
-            $($(".Fajr").get(dayNumber)).text(data[i]["Fajr"]);
-            $($(".Sunrise").get(dayNumber)).text(data[i]["Sunrise"]);
-            $($(".Zuhr").get(dayNumber)).text(data[i]["Zuhr"]);
-            $($(".Asr").get(dayNumber)).text(data[i]["Asr"]);
-            $($(".Maghrib").get(dayNumber)).text(data[i]["Maghrib"]);
-            $($(".Esha").get(dayNumber)).text(data[i]["Esha"]);
+            $($(".Date").get(dayNumber)).html(getDateToShow(data[i]["Month"], data[i]["Date"]));
+            $($(".Day").get(dayNumber)).html(getDayToShow(data[i]["Month"], data[i]["Date"]));
+            $($(".Fajr").get(dayNumber)).html(data[i]["Fajr"]);
+            $($(".Sunrise").get(dayNumber)).html(data[i]["Sunrise"]);
+            $($(".Zuhr").get(dayNumber)).html(data[i]["Zuhr"]);
+            $($(".Asr").get(dayNumber)).html(data[i]["Asr"]);
+            $($(".Maghrib").get(dayNumber)).html(data[i]["Maghrib"]);
+            $($(".Esha").get(dayNumber)).html(data[i]["Esha"]);
 
-            $($(".fIqama").get(dayNumber)).text(data[i]["fIqama"]);
-            $($(".zIqama").get(dayNumber)).text(data[i]["zIqama"]);
-            $($(".aIqama").get(dayNumber)).text(data[i]["aIqama"]);
-            $($(".mIqama").get(dayNumber)).text(data[i]["mIqama"]);
-            $($(".eIqama").get(dayNumber)).text(data[i]["eIqama"]);
+            // $($(".fIqama").get(dayNumber)).html(data[i]["fIqama"]);
+            // $($(".zIqama").get(dayNumber)).html(data[i]["zIqama"]);
+            // $($(".aIqama").get(dayNumber)).html(data[i]["aIqama"]);
+            // $($(".mIqama").get(dayNumber)).html(data[i]["mIqama"]);
+            // $($(".eIqama").get(dayNumber)).html(data[i]["eIqama"]);
+
+            $($(".fIqama").get(dayNumber)).html(addBoldOnIfTimeChange(i, data, "fIqama"));
+            $($(".zIqama").get(dayNumber)).html(addBoldOnIfTimeChange(i, data, "zIqama"));
+            $($(".aIqama").get(dayNumber)).html(addBoldOnIfTimeChange(i, data, "aIqama"));
+            $($(".mIqama").get(dayNumber)).html(data[i]["mIqama"]);
+            $($(".eIqama").get(dayNumber)).html(addBoldOnIfTimeChange(i, data, "eIqama"));
         }
     }
     cleanUp(dayNumber);
 }
 
-function getDateToShow(month, date){
+function getDateToShow(month, date) {
     return months[month] + " " + date;
 }
 
-function getDayToShow(month, date){
-    return days[new Date(selectedDate.getFullYear(), month-1, date).getDay()];
+function getDayToShow(month, date) {
+    return days[new Date(selectedDate.getFullYear(), month - 1, date).getDay()];
 }
 
-function cleanUp(dayNumber){
+function cleanUp(dayNumber) {
     $("tbody").html($("tbody").html().replace(/am/ig, '').replace(/pm/ig, ''));
     const info = tobedeleted["data"]["Info"]["data"][0];
     masjidname.innerHTML = info["name"];
     masjidaddress.innerHTML = info["address"] + " " + info["city"] + ", " + info["state"] + " " + info["zip"];
     //hiding extra rows
-    while (dayNumber<31) {//if true there are rows to hide
+    while (dayNumber < 31) {//if true there are rows to hide
         dayNumber++;
         $($(".Date").get(dayNumber)).parent().hide();
     }
 }
 
+function addBoldOnIfTimeChange(i, data, col) {
+    let h = i - 1;//h is yester day i is today
+    if (h == -1) {
+        h = 364;
+    }
+    if (data[h][col] !== data[i][col]) {
+        console.log(data[h][col] , data[i][col]);
+        return "<b>"+data[i][col]+"</b>";
+    }
+    return data[i][col];
+}
+
 /* ui triggers */
 
-
-
-function monthChanged(){
+function monthChanged() {
     params.set("month", document.getElementById("selectedMonth").value);
     let newUrl = location.protocol + "//" + location.hostname;
-    if(location.port != ""){
+    if (location.port != "") {
         newUrl += ":" + location.port;
     }
-    newUrl += location.pathname + "?"+ params.toString();
+    newUrl += location.pathname + "?" + params.toString();
     window.location = newUrl;
 }
